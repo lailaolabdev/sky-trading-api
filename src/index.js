9c75efada2dc6e54d574cb5ba7789467e7f739d0
Done-Yang <doneyang2003@gmail.com>
2025-01-12 14:12:19 -0700

    complate CI&CD to the api

--- src/index.js
+++ src/index.js
@@ -45,5 +45,5 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 const PORT = process.env.PORT || 8000;
 const server = app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Sky-Trading-API server is running... on port ${PORT}`);
 })
