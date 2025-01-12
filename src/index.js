0cb41fe2e1fbe70a61cf5dba8c89c58305c0f8aa
Done-Yang <doneyang2003@gmail.com>
2025-01-12 14:21:27 -0700

    complated CI&CD to the api

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ const testimonialRoutes = require("./routes/testimonial");
 
 /// routes endpoints
 app.get("/", (req, res) => {
-    res.send("Server is running...");
+    res.send("Sky-Trading-API Server is running...");
 });
 app.use("/api/v1/staff", staffRoutes);
 app.use("/api/v1/login", loginRoutes);
@@ -45,5 +45,5 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 const PORT = process.env.PORT || 8000;
 const server = app.listen(PORT, () => {
-    console.log(`Sky-Trading-API server is running... on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 })
